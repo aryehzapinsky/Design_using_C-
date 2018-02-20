b1b499278ef5e30bd32a62a27989a9095f8392e2
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-02-20 11:00:06 -0500

    vector_list_comparision working
    starting on map insertions and deletions

--- vectorlist.cpp
+++ vectorlist.cpp
@@ -9,18 +9,22 @@
 using namespace std;
 
 template<class Sequence, typename T>
-void special_insert(Sequence s, T &container) {
+void special_insert(Sequence s, T& container) {
     auto sit = s.begin();
     container.insert(container.begin(), *sit);
-    for (sit++; sit != s.end(); sit++) {
+    sit++;
+
+    while (sit != s.end()) {
         auto cit = container.begin();
-        for (; *sit > *cit; cit++);
+        while((*sit > *cit) && cit!=container.end())
+            cit++;
         container.insert(cit, *sit);
+        sit++;
     }
 }
 
 template<class Sequence, typename T>
-void special_remove(Sequence s, T &container) {
+void special_remove(Sequence s, T& container) {
     for (auto i : s) {
         auto cit = container.begin();
         for (int j = 0; j != i; j++, cit++);
@@ -41,59 +45,69 @@ void generate_removal_ints(int k, vector<int> &removal) {
     random_device r;
 
     default_random_engine generator(r());
-    for (auto i = 0; i < k; i++) {
-        uniform_int_distribution<int> distribution(0, k-i);
+    for (auto i = k-1; i > 0; i--) {
+        uniform_int_distribution<int> distribution(0, i);
         removal.push_back(distribution(generator));
     }
+    removal.push_back(0);
 
 }
 
+template<typename T, typename U, typename V>
+chrono::duration<double> run_experiment (T &seq, U &removal, V &container) {
+    auto start = chrono::system_clock::now();
+    special_insert(seq, container);
+    special_remove(removal, container);
+    auto end = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds = end-start;
 
+    return elapsed_seconds;
+}
+
+void vector_list_comparision(int start_num) {
+    cout << "\tList\tVector\n";
+    for (auto x = 0; x < 10; x ++) {
+        auto num_elements = start_num + start_num * x;
+        for (auto attempt = 0; attempt < 3; attempt++) {
+            cout << num_elements << "\t";
+            auto seq = unordered_set<int>();
+            generate_rand_ints(num_elements, seq);
+
+            vector<int> removal;
+            generate_removal_ints(x, removal);
+
+            list<int> l;
+            auto elapsed_seconds = run_experiment(seq, removal, l);
+
+            cout<< elapsed_seconds.count() << "\t";
+
+            vector<int> v;
+            elapsed_seconds = run_experiment(seq, removal, v);
+
+            cout<< elapsed_seconds.count() << '\n';
+        }
+    }
+}
+
+void run_map_experiment(int start_num) {
+    cout << "\tMap\n";
+    for (auto x = 0; x < 10; x ++) {
+        auto num_elements = start_num + start_num * x;
+        for (auto attempt = 0; attempt < 3; attempt++) {
+            cout << num_elements << "\t";
+            auto seq = unordered_set<int>();
+            generate_rand_ints(num_elements, seq);
+
+            vector<int> removal;
+            generate_removal_ints(x, removal);
+
+
+            cout<< elapsed_seconds.count() << "\n";
+        }
+    }
+}
 
 int main()
 {
-    cout<<"Hi";
-
-    // ofstream results{"results.csv"};
-    // if (!results) {
-    //     cout<<"Error 'opening results.csv'\n";
-    //     abort();
-    // }
-    // for (auto x = 0; x < 10; x++) {
-    //     auto sequence = unordered_set<int>();
-    //     generate_rand_ints(x*100000, sequence);
-
-
-    // }
-    // auto sequence = unordered_set<int>();
-    // generate_rand_ints(100000, sequence);
-
-    auto seq = unordered_set<int>();
-    cout<<seq.size();
-    generate_rand_ints(10, seq);
-    vector<int> removal = {2, 1, 0, 0};
-
-    for (auto i : seq)
-        cout<<i;
-
-
-    auto start = chrono::system_clock::now();
-    list<int> l;
-    special_insert(seq, l);
-    for (auto i : l)
-        cout<<i;
-    special_remove(removal, l);
-    auto end = chrono::system_clock::now();
-    auto elapsed_seconds = end-start;
-
-    cout<<"List: " << elapsed_seconds.count() << '\n';
-
-    start = chrono::system_clock::now();
-    vector<int> v;
-    special_insert(seq, v);
-    special_remove(removal, v);
-    end = chrono::system_clock::now();
-    elapsed_seconds = end-start;
-
-    cout<<"Vector: " << elapsed_seconds.count() << '\n';
+    vector_list_comparision(10);
 }
